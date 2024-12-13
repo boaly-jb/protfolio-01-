a813495919955e750a3760071e2a3606e57260af
CRAZY Designer <jababoaly@gmail.com>
2024-12-13 12:37:11 +0600

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,147 @@
+let heror = document.querySelector('.herror')
+
+heror.style.height = "100vh"
+heror.style.background = ""
+
+
+
+
+
+
+// // Mood START.........................................
+
+// let moodBtn = document.querySelector('.mood')
+// // let moodBtn = document.querySelector('.mood')
+// let moodBox = document.querySelector('.event2-box')
+
+// function moodChange() {
+//     moodBox.classList.toggle('darkMood');
+//     // console.log('This Button Clicked')
+// }
+// moodBtn.addEventListener('click', moodChange)
+
+// // !      Mood END....................................................
+
+
+
+
+// // Side Bar START.........................................
+
+// let menuBtn = document.querySelector('.menu')
+
+// let sideBar = document.querySelector('.side-bar')
+
+// let closeSideBar = document.querySelector('.closeBtn')
+
+
+// function openSideBar(){
+//     sideBar.classList.add('active')
+// }
+
+// menuBtn.addEventListener('click',  openSideBar)
+
+// function sideBarClosed(event) {
+   
+//     if (
+//             event.target.classList.contains("side-bar") ||
+//             event.target.classList.contains("closeBtn")
+//           ) {
+//             sideBar.classList.remove("active");
+//           }
+
+// }
+// closeSideBar.addEventListener('click', sideBarClosed)
+
+// sideBar.addEventListener('click', sideBarClosed)
+
+
+// //  !      Side Bar END...........................................
+
+
+
+
+
+
+
+
+
+// // Mouse Cursore TART................................
+// let cursore = document.querySelector('.cursor')
+// let cursoreSm = document.querySelector('.cursoreSM')
+// let cBox = document.querySelector('.event4-box')
+
+
+// function cursoreMove (event) {
+//     let cursorLeft = event.pageX - cBox.offsetLeft //only inside the box 
+//     let cursorTop = event.pageY - cBox.offsetTop
+
+//    cursore.style.top = `${cursorTop}px`
+//    cursore.style.left = `${cursorLeft}px`
+//    cursoreSm.style.top = `${cursorTop}px`
+//    cursoreSm.style.left = `${cursorLeft}px`
+
+// //    console.log(event)
+// }
+
+// cBox.addEventListener('mousemove', cursoreMove)
+
+
+
+
+
+// function hoverEffect(event) {
+//     cursore.style.height = "100px";
+//     cursore.style.width = "100px";
+//     cursore.style.backgroundColor = "rgba(255, 165, 0, 0.2)";
+//     cursore.style.borderColor = "transparent";
+// }
+
+// function removeHoverEffect(event) {
+//     cursore.style.height = "30px";
+//     cursore.style.width = "30px";
+//     cursore.style.backgroundColor = "transparent";
+//     cursore.style.borderColor = "orange";
+// }
+
+// // cBox.addEventListener('mousemove', cursoreMove);
+
+// // Add hover effect for anchor tags
+// let links = document.querySelectorAll('.event-box a');
+// links.forEach(link => {
+//     link.addEventListener('mouseover', hoverEffect);
+//     link.addEventListener('mouseleave', removeHoverEffect);
+// });
+
+// //! Mouse Cursore END ...........................
+
+
+
+// // todo Loop is start from here.........
+
+
+
+
+// let prgf = document.querySelectorAll('.five-content p')
+
+// for(let i = 0; i <= (prgf.length-1); i++ ) {
+//    prgf[i].style.color = 'rgb(9 133 155)'
+// }
+
+
+// // todo Loop is END from here.........
+
+
+// let faqBoxs = document.querySelector('.faq-body')
+// let faqBtn = document.querySelector('.faq-hadder button')
+
+
+// function openFaq() {
+//     faqBoxs.classList.toggle('active-faq')
+// }
+
+// for (faqBox ) {
+
+// }
+
+// faqBtn.addEventListener('click', openFaq)
+
